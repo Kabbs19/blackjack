1e047f5e05514be05622ec3c121daf9f80a54f30
Kabbs19 <77408435+Kabbs19@users.noreply.github.com>
2025-12-19 23:00:28 +0000

    Add age and firstCard secondCard js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-function()
+// age
+
+let age = 22;
+
+if (age < 21) {
+    console.log("You can not play the game!")
+} else {
+    console.log("Welcome!")
+}
+
+let firstCard = 2;
+let secondCard = 3;
+
+let sum = firstCard + secondCard;
+
+if (sum < 21) {
+    console.log("Do you want another card?")
+} else if (sum === 21) {
+    console.log("You've got blackjack!")
+} else { 
+    console.log("You're out of the game!")
+}
